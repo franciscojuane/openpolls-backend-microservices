0c9363ebc0267887e7340a5b667febf5fb0a0f27
Francisco Juane <franciscojuaneutn@gmail.com>
2025-05-17 16:20:43 -0300

    Submissions base microservice added

---
+++ submissions-service/src/main/java/com/francisco/submissions_service/SubmissionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.francisco.submissions_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmissionsServiceApplication.class, args);
+	}
+
+}
---
+++ submissions-service/src/test/java/com/francisco/submissions_service/SubmissionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.submissions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
