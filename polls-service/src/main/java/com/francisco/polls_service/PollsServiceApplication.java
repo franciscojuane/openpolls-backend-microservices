eda01e11fb0876af33c7e00d8105e66993e95811
Francisco Juane <franciscojuaneutn@gmail.com>
2025-05-17 16:19:48 -0300

    Polls base microservice added

---
+++ polls-service/src/main/java/com/francisco/polls_service/PollsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.francisco.polls_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsServiceApplication.class, args);
+	}
+
+}
