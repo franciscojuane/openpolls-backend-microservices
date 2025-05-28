fdff39bb6a01c6191abb112939b37346f2910774
Francisco Juane <franciscojuaneutn@gmail.com>
2025-05-28 13:52:55 -0300

    Unused code removed

--- auth-service/src/main/java/com/francisco/auth_service/v1/security/JwtService.java
+++ auth-service/src/main/java/com/francisco/auth_service/v1/security/JwtService.java
@@ -63,18 +63,6 @@ public class JwtService {
 		return secretKey;
 	}
 
-	// quitar
-	/*
-	 * public boolean isTokenValid(String token, UserDetails userDetails) { final
-	 * String username = extractUsername(token); return
-	 * (username.equals(userDetails.getUsername())) && !isTokenExpired(token); }
-	 */
-
-	/*
-	 * public String extractUsername(String token) { return extractClaim(token,
-	 * claim -> claim.getSubject()); }
-	 */
-
 	private boolean isTokenExpired(String token) {
 		return extractExpiration(token).before(new Date());
 	}
