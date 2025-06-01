20d1d6250571cc40f13cbe3103b4b7a541db3ce4
Francisco Juane <franciscojuaneutn@gmail.com>
2025-06-01 18:52:37 -0300

    UserService and USerServiceClient finished

--- auth-service/src/main/java/com/francisco/auth_service/v1/external/UserService.java
+++ auth-service/src/main/java/com/francisco/auth_service/v1/external/UserService.java
@@ -1,9 +1,14 @@
 package com.francisco.auth_service.v1.external;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.francisco.auth_service.v1.external.dto.UserResponse;
 
 public class UserService {
 
+	@Autowired
+	UserServiceClient userServiceClient;
+	
 	public UserResponse findUserByEmail(String email) {
 		
 	}
--- auth-service/src/main/java/com/francisco/auth_service/v1/external/UserServiceClient.java
+++ auth-service/src/main/java/com/francisco/auth_service/v1/external/UserServiceClient.java
@@ -1,5 +1,14 @@
 package com.francisco.auth_service.v1.external;
 
-public class UserServiceClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.francisco.auth_service.v1.external.dto.UserResponse;
+
+@FeignClient("user-service")
+public interface UserServiceClient {
+
+	@GetMapping("/findUserByEmail/{email}")
+	public UserResponse findUserByEmail(@PathVariable String email);
 }
