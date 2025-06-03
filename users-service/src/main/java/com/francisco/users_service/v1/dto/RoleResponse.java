169008893f9c9edd95e81c2ba87a5b296c5d5aa8
Francisco Juane <franciscojuaneutn@gmail.com>
2025-06-03 19:50:12 -0300

    Mappers for roles finished

--- users-service/src/main/java/com/francisco/users_service/v1/dto/RoleResponse.java
+++ users-service/src/main/java/com/francisco/users_service/v1/dto/RoleResponse.java
@@ -3,8 +3,6 @@ package com.francisco.users_service.v1.dto;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.francisco.users_service.v1.model.Permission;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,5 +16,5 @@ public class RoleResponse {
 
 	private String name;
 	
-	private Set<Permission> permissions = new HashSet<>();
+	private Set<PermissionResponse> permissions = new HashSet<>();
 }
--- users-service/src/main/java/com/francisco/users_service/v1/dto/mappers/RoleMapper.java
+++ users-service/src/main/java/com/francisco/users_service/v1/dto/mappers/RoleMapper.java
@@ -1,8 +1,27 @@
 package com.francisco.users_service.v1.dto.mappers;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.francisco.users_service.v1.dto.PermissionResponse;
+import com.francisco.users_service.v1.dto.RoleResponse;
+import com.francisco.users_service.v1.model.Permission;
+import com.francisco.users_service.v1.model.Role;
 
 @Component
 public class RoleMapper {
 
+	@Autowired
+	PermissionMapper permissionMapper;
+	
+	public RoleResponse roleToRoleResponse(Role role) {
+		Set<PermissionResponse> permissions = new HashSet<>();
+		for (Permission permission : role.getPermissions()) {
+			permissions.add(permissionMapper.permissionToPermissionResponse(permission));
+		}
+		return RoleResponse.builder().permissions(permissions).name(role.getName()).build();
+	}
 }
