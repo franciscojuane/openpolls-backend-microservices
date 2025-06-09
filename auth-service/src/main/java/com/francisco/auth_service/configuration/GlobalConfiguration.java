d0f3dbc82e4f092e9140bb3290d902d5292e010a
Francisco Juane <franciscojuaneutn@gmail.com>
2025-06-09 14:35:27 -0300

    Removed @EnableFeignClients from GlobalConfiguration class and added PasswordEncoder bean

--- auth-service/src/main/java/com/francisco/auth_service/configuration/GlobalConfiguration.java
+++ auth-service/src/main/java/com/francisco/auth_service/configuration/GlobalConfiguration.java
@@ -1,10 +1,16 @@
 package com.francisco.auth_service.configuration;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableFeignClients
 public class GlobalConfiguration {
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
