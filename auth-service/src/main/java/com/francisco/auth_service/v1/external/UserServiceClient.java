7aada3f9394ace1843ac5dff97930556e057a671
Francisco Juane <franciscojuaneutn@gmail.com>
2025-06-09 14:38:38 -0300

    Fixed routes for UserServiceClient

--- auth-service/src/main/java/com/francisco/auth_service/v1/external/UserServiceClient.java
+++ auth-service/src/main/java/com/francisco/auth_service/v1/external/UserServiceClient.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.francisco.auth_service.v1.external.dto.UserDetailsResponse;
 import com.francisco.auth_service.v1.external.dto.UserResponse;
 
-@FeignClient("user-service")
+@FeignClient("users-service")
 public interface UserServiceClient {
 
-	@GetMapping("/findUserByEmail/{email}")
+	@GetMapping("/v1/users/findUserByEmail/{email}")
 	public UserResponse findUserByEmail(@PathVariable String email);
 	
-	@GetMapping("/findUserDetailsByEmail/{email}")
+	@GetMapping("/v1/users/findUserDetailsByEmail/{email}")
 	public UserDetailsResponse findUserDetailsByEmail(@PathVariable String email);
 }
