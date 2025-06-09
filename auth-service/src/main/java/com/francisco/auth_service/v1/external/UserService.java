53d07de5226c04a571f9cd0bca4b3b13673d9d01
Francisco Juane <franciscojuaneutn@gmail.com>
2025-06-08 21:56:26 -0300

    Jwt params added to application.yml at Auth Service. UserService and UserServiceClient made components

--- auth-service/src/main/java/com/francisco/auth_service/v1/external/UserService.java
+++ auth-service/src/main/java/com/francisco/auth_service/v1/external/UserService.java
@@ -1,10 +1,12 @@
 package com.francisco.auth_service.v1.external;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.francisco.auth_service.v1.external.dto.UserDetailsResponse;
 import com.francisco.auth_service.v1.external.dto.UserResponse;
 
+@Component
 public class UserService {
 
 	@Autowired
