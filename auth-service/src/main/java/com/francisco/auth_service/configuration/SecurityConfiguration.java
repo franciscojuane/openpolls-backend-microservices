f0a1a6e530e9ed3206d32a14bdc5cf92466e053f
Francisco Juane <franciscojuaneutn@gmail.com>
2025-06-11 14:43:33 -0300

    Previous Security Configuration removed

--- auth-service/src/main/java/com/francisco/auth_service/configuration/SecurityConfiguration.java
+++ auth-service/src/main/java/com/francisco/auth_service/configuration/SecurityConfiguration.java
@@ -7,17 +7,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-	/*@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http
-	        .csrf(csrf -> csrf.disable())
-	        .authorizeHttpRequests(auth -> auth
-	            .requestMatchers("/public/**").permitAll()
-	            .anyRequest().authenticated()
-	        )
-	        .oauth2ResourceServer(oauth2 -> oauth2.jwt());
-	    return http.build();
-	}*/
+
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
